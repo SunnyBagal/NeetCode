4c738a7861089520ac4b4280054fab00fb6d5ddc
Sinister <sunnybagal.1110@gmail.com>
2026-08-18 22:27:09 +0530

    Palindrom Partioning

--- 9 Advance Recursion (Backtracking)/Palindrome Partitioning.py
+++ 9 Advance Recursion (Backtracking)/Palindrome Partitioning.py
@@ -1,2 +1,24 @@
 s = "aab"
 
+res = []
+part = []
+
+def dfs(i):
+  if i >= len(s):
+    res.append(part.copy())
+    return
+
+  for j in range(i, len(s)):
+    if Pali(s, i, j):
+      part.append(s[i: j + 1])
+      dfs(j + 1)
+      part.pop()
+
+dfs(0)
+
+def Pali(s, l, r):
+  while l < r:
+    if s[l] != s[r]:
+      return False
+    l, r = l + 1, r -1
+  return True
